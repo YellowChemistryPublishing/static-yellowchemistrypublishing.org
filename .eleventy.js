c7ec7dbdcd62092edab0c9792314abfc991a8d69
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-05-11 13:27:12 +1000

    Add metadata include.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
 import fs from "node:fs";
 
 export default function (eleventyConfig) {
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
     eleventyConfig.addFilter("embed64", function (value) {
         return fs.readFileSync(value, "base64");
+    });
+    eleventyConfig.addFilter("obj2JSON", function (value) {
+        return JSON.stringify(value);
     });
 
     fs.rmSync("generated/", { recursive: true, force: true });
