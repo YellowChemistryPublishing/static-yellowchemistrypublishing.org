496962f8c402eb9fc8c4c537095459963ef952a6
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-05-18 00:04:46 +1000

    Make hello world page a template.

---
+++ _data/hello_world.js
@@ -0,0 +1,14 @@
+module.exports = () => {
+    ret = [
+        {
+            name: "hello_api",
+            content: `
+            <p>
+                Welcome to the standard test page for <a href="https://api.yellowchemistrypublishing.org">api.yellowchemistrypublishing.org</a>. If you see this page, it means that the server is running and the API is live.
+                This API is primarily used by <a href="https://cms.yellowchemistrypublishing.org" target="_blank">cms.yellowchemistrypublishing.org</a>, but it is also available for public use, should you wish.
+                You can use the API to access various resources and services provided by Yellow Chemistry Publishing.
+            </p>`
+        }
+    ];
+    return ret;
+};
